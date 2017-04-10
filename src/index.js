192ec2889d98095ac3260f5cabd94f96b4911f21
fzred <1179750402@qq.com>
2017-04-10 20:10:00 +0800

    构建

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 const { praseExpression } = require('./parse')
 
 function calc(str) {
-    console.log(praseExpression(str))
+    return praseExpression(str)
 }
-
-calc(`0.1*0.2`)
-calc(`0.1*(1+0.2)`)
-calc(' 1+1 * 0.2 + 0.4')
-//export default calc
 
 module.exports = calc
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack')
+const join = require('path').join
+
+const product = process.env.BABEL_ENV === 'product'
+
+let filename = 'calc.js';
+let path = join(__dirname, 'lib');
+const plugins = []
+const uglify = new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, drop_console: true } })
+
+if (product) {
+    plugins.push(uglify)
+    filename = 'calc.min.js'
+    path = join(__dirname, 'dist')
+}
+
+const config = {
+    context: __dirname,
+    entry: join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path,
+        filename: filename,
+        library: 'calc',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    plugins: plugins,
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { // 参数
+                    presets: ['es2015', 'stage-2'],
+                    plugins: ['transform-runtime'],
+                }
+            }
+        ]
+    },
+}
+
+module.exports = config
