e8211f839a1d76f264d8c2316aeb6f8cda07a7fe
fzred <1179750402@qq.com>
2017-08-17 19:00:49 +0800

    开放运算API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const { parseExpression } = require('./parse')
+const precisionCalc = require('./precisionCalc')
 
 function calc(str) {
     return parseExpression(str)
+}
+
+for (const key in precisionCalc) {
+    calc[key] = precisionCalc[key]
 }
 
 module.exports = calc
